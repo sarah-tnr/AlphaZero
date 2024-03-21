b62a9b7be15ebd71c8523848437c70a9bcc6da25
PascalGraf95 <pascal.graf@hs-heilbronn.de>
2024-03-21 14:05:14 +0100

    keras network

--- workspace/src/KulamiNNet.py
+++ workspace/src/KulamiNNet.py
@@ -1,9 +1,21 @@
-from src.KulamiLogic import Board
+# from src.KulamiLogic import Board
 
+import os
+os.environ["KERAS_BACKEND"] = "torch"
+from KulamiGame import KulamiGame
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 #import torch.optim as optim
+from torchvision import models
+from torchsummary import summary
+from dotdict import dotdict
+import keras
+from keras.models import *
+from keras.layers import Conv2D, Input, BatchNormalization, Flatten, Dense, Dropout, Softmax, Concatenate, Add
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+
 
 class KulamiNNet(nn.Module):
     "Define Neural Network for Kulami Game."
@@ -12,44 +24,55 @@ class KulamiNNet(nn.Module):
         # game params
         # Input for NN is Board wit 6 levels
         self.board_x, self.board_y, self.board_z = game.getBoardSize()
+        print(self.board_x, self.board_y, self.board_z)
         # Output of NN is action
         self.action_size = game.getActionSize()
         self.args = args
 
         super(KulamiNNet, self).__init__()
-        self.conv1 = nn.Conv3d(1, args.num_channels, 3, stride=1, padding=1)
-        self.conv2 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
-        self.conv3 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1)
-        self.conv4 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1)
+        self.input_boards = Input(shape=(self.board_x, self.board_y, self.board_z))
+        self.conv1 = Conv2D(args.num_channels, activation="relu", kernel_size=3, strides=1, padding="same")(self.input_boards)
+        self.bn1 = BatchNormalization()(self.conv1)
+        self.conv2 = Conv2D(args.num_channels, activation="relu", kernel_size=3, strides=1, padding="same")(self.bn1)
+        self.bn2 = BatchNormalization()(self.conv2)
+        self.conv3 = Conv2D(args.num_channels, activation="relu", kernel_size=3, strides=1, padding="valid")(self.bn2)
+        self.bn3 = BatchNormalization()(self.conv3)
+        self.conv4 = Conv2D(args.num_channels, activation="relu", kernel_size=3, strides=1, padding="valid")(self.bn3)
+        self.bn4 = BatchNormalization()(self.conv4)
 
-        self.bn1 = nn.BatchNorm3d(args.num_channels)
-        self.bn2 = nn.BatchNorm3d(args.num_channels)
-        self.bn3 = nn.BatchNorm3d(args.num_channels)
-        self.bn4 = nn.BatchNorm3d(args.num_channels)
+        self.flatten = Flatten()(self.bn4)
 
-        self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4)*(self.board_z-4), 1024)
-        self.fc_bn1 = nn.BatchNorm1d(1024)
+        self.fc1 = Dense(1024, activation="relu")(self.flatten)
+        self.fc_bn1 = BatchNormalization()(self.fc1)
+        self.fc_dropout1 = Dropout(args.dropout)(self.fc_bn1)
 
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc_bn2 = nn.BatchNorm1d(512)
+        self.fc2 = Dense(512, activation="relu")(self.fc_dropout1)
+        self.fc_bn2 = BatchNormalization()(self.fc2)
+        self.fc_dropout2 = Dropout(args.dropout)(self.fc_bn2)
 
-        self.fc3 = nn.Linear(512, self.action_size)
+        self.fc3 = Dense(self.action_size, activation="softmax", name="policy")(self.fc_dropout2)
+        self.fc4 = Dense(1, activation="tanh", name="value")(self.fc_dropout2)
 
-        self.fc4 = nn.Linear(512, 1)
+        self.model = Model(inputs=self.input_boards, outputs=[self.fc3, self.fc4])
+        self.model.summary()
 
-    def forward(self, s):
-        #                                                           s: batch_size x board_x x board_y x board_z
-        s = s.view(-1, 1, self.board_x, self.board_y, self.board_z)  # batch_size x 1 x board_x x board_y x board_z
-        s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y x board_z
-        s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y x board_z
-        s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2) x (board_z-2)
-        s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4) x (board_z-4)
-        s = s.view(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4)*(self.board_z-4))
+    # (batch size, 224, 224, 3)
+    # (batch size, 3, 224, 244)
+    # np.transpose(x, (0, 3, 1, 2))
 
-        s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)  # batch_size x 1024
-        s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.args.dropout, training=self.training)  # batch_size x 512
 
-        pi = self.fc3(s)                                                                         # batch_size x action_size
-        v = self.fc4(s)                                                                          # batch_size x 1
+if __name__ == '__main__':
+    args = dotdict({
+        'lr': 0.001,
+        'dropout': 0.3,
+        'epochs': 10,
+        'batch_size': 64,
+        'cuda': torch.cuda.is_available(),
+        'num_channels': 256,
+    })
 
-        return F.log_softmax(pi, dim=1), torch.tanh(v)
+    game = KulamiGame()
+    network = KulamiNNet(args=args, game=game)
+    # summary(network, (6, 8, 8), batch_size=1)
+    # test_tensor = torch.zeros((1, 6, 8, 8))
+    # output = network(test_tensor)
